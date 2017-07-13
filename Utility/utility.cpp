c41b377ff16c018b96550f5c9bc4089d8a6b7813
Taniey.Ju <taniey@163.com>
2017-07-13 22:23:05 +0800

    update the projects setting.

--- Utility/utility.cpp
+++ Utility/utility.cpp
@@ -8,13 +8,16 @@
 //get the app path
 StdString GetAppPath(void)
 {
-	TCHAR bufPath[MAX_PATH] = _T("");
-	::GetModuleFileName(NULL, bufPath, MAX_PATH);
+	static StdString strAppPath;
+	if (strAppPath.empty()) {
+		TCHAR bufPath[MAX_PATH] = _T("");
+		::GetModuleFileName(NULL, bufPath, MAX_PATH);
 
-	StdString strAppPath(bufPath);
-	//csAppPath = csAppPath.Left(csAppPath.ReverseFind(_T('\\')));
-	//strAppPath = strAppPath.substr(0, strAppPath.rfind(_T("\\")));
+		StdString strAppPath(bufPath);
+		//csAppPath = csAppPath.Left(csAppPath.ReverseFind(_T('\\')));
+		strAppPath = strAppPath.substr(0, strAppPath.rfind(_T("\\"))+1);
+	}
 
-	return strAppPath.substr(0, strAppPath.rfind(_T("\\"))+1);;
+	return strAppPath;
 }
 
